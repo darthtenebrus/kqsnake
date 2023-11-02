50c7796cc4fdd9b5fd18b9b236825ebff591e8fc
esorochinskiy <evgeny.sorochinskiy@iqr.su>
2023-11-02 10:28:25 +0300

    images

---
+++ mainwindow_ru.ts
@@ -0,0 +1,48 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="20"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="88"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="88"/>
+        <source>Snake Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="52"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="90"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="90"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="73"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="91"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="91"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="85"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="89"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="89"/>
+        <source>New Game</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="snakegame.cpp" line="198"/>
+        <source>You lost</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="snakegame.cpp" line="198"/>
+        <source>Sorry, you lost</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
