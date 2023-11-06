807a85f47cff2215d4a6f874a5944140d372c5b8
esorochinskiy <evgeny.sorochinskiy@iqr.su>
2023-11-06 12:11:52 +0300

    translation

---
+++ mainwindow_de.ts
@@ -0,0 +1,98 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="20"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="117"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="111"/>
+        <source>Snake Game</source>
+        <translation>Schlangespiel</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="52"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="128"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="116"/>
+        <source>toolBar</source>
+        <translation>toolBar</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="75"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="129"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="117"/>
+        <source>Game</source>
+        <translation>Spiel</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="82"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="130"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="118"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="99"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="118"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="112"/>
+        <source>New Game</source>
+        <translation>Neues Spiel</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="102"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="120"/>
+        <source>Alt+N</source>
+        <translation>Alt+N</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="107"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="122"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="113"/>
+        <source>About Qt</source>
+        <translation>Über Qt</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="112"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="123"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="114"/>
+        <source>About</source>
+        <translation>Über die Applikation</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="121"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="124"/>
+        <location filename="cmake-build-release/kqsnake_autogen/include/ui_mainwindow.h" line="115"/>
+        <source>Start/Stop Game</source>
+        <translation>Start/Stop Spiel</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="124"/>
+        <location filename="cmake-build-debug/kqsnake_autogen/include/ui_mainwindow.h" line="126"/>
+        <source>Alt+S</source>
+        <translation>Alt+S</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="28"/>
+        <source>Snake Movement Speed</source>
+        <translation>Schlangelaufgeschwindigkeit</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="50"/>
+        <source>A simple Game Of Snake</source>
+        <translation>Die einfache Schlangespiel</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="snakegame.cpp" line="199"/>
+        <source>You lost</source>
+        <translation>Verloren</translation>
+    </message>
+    <message>
+        <location filename="snakegame.cpp" line="199"/>
+        <source>Sorry, you lost</source>
+        <translation>Sie haben verloren</translation>
+    </message>
+</context>
+</TS>
